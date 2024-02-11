1a285ad0e9207a21c9faaf2e5e8351132dbb3307
Sourab <sourabmayya@gmail.com>
2024-02-11 13:25:44 +0530

    added comments

---
+++ server/middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        let decodedData;
+        if (token) {
+            decodedData = jwt.verify(token, process.env.SECRET_FOR_TOKEN);
+        }
+
+        req.userId = decodedData?.id;
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default auth; 
---
+++ server/routes/posts.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { getPost, getPostsBySearch, getPosts, createPost, updatePost, deletePost, likePost, commentPost } from '../controllers/posts.js';
+import auth from '../middleware/auth.js';
+const router = express.Router();
+
+router.get('/search', getPostsBySearch);
+router.get('/', getPosts);
+router.get('/:id', getPost);
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost', auth, commentPost)
+//person must be able to CRUD only his posts
+export default router;
