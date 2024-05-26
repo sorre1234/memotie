b4948ac671647faddce0dea0cfa28aa938194781
sorre1234 <106836961+sorre1234@users.noreply.github.com>
2024-05-26 09:47:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import helmet from 'helmet';
+// import helmet from 'helmet';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 
